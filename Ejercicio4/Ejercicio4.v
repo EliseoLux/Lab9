a716245a27700f19ffa2f36277be01921f56fa7f
EliseoLux <68084030+EliseoLux@users.noreply.github.com>
2020-10-31 11:05:47 -0600

    Completado

---
+++ Ejercicio4/Ejercicio4.v
@@ -0,0 +1,11 @@
+//Juan Eliseo Lux Cajas
+//Lab9
+//Ejecicio 4
+
+module bft(input enable, input [3:0]d, output logic [3:0]q);
+	always @ (*)
+		if (enable == 0)
+			q <= 1'bz;
+		else
+			q <= d;
+endmodule
